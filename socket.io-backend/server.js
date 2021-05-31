b947fd665f9bacf3c024b3e84b9e0008de50fb66
Azhar Ali <azharofficial599@gmail.com>
2021-05-31 12:08:29 +0530

    inital-commit

---
+++ socket.io-backend/server.js
@@ -0,0 +1,31 @@
+const io = require("socket.io")();
+const messageHandler = require("./handlers/message.handler");
+
+let currentUserId = 2;
+const users = {};
+
+function createUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
+
+io.on("connection", socket => {
+  console.log("a user connected!");
+  console.log(socket.id);
+  users[socket.id] = { userId: currentUserId++ };
+  socket.on("join", username => {
+    users[socket.id].username = username;
+    users[socket.id].avatar = createUserAvatarUrl();
+    messageHandler.handleMessage(socket, users);
+  });
+  socket.on("action", action => {
+    switch (action.type) {
+      case "server/hello":
+        console.log("Got hello event", action.data);
+        socket.emit("action", { type: "message", data: "Good day!" });
+    }
+  });
+});
+
+io.listen(3001);
